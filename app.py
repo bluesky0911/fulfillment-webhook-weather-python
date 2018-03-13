58d7ac02d1772a013b4a282be59dfeb2b589a45d
Hyeongcheol Kim <bluesky0911@users.noreply.github.com>
2018-03-13 13:19:52 +0800

    Update app.py
    Testing how fulfilment works in dialogflow

--- app.py
+++ app.py
@@ -39,10 +39,11 @@ def webhook():
 
     print("Request:")
     print(json.dumps(req, indent=4))
+    print("TEST PRINTING~~~~~")
 
-    res = processRequest(req)
+    #res = processRequest(req)
 
-    res = json.dumps(res, indent=4)
+    #res = json.dumps(res, indent=4)
     # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
